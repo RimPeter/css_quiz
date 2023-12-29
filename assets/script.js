0fd4b2dd3018a833d91822dc242b24aa42734ed6
RimPeter <hull@localappliancerentals.co.uk>
2023-12-29 09:41:17 +0000

    adding some javascript code for the question div

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,23 @@
 //         console.log(i);
 //     }, i * 1000);
 // }
+
+let table = {
+    "cat":4,
+    "centipede":100,
+    "chicken":2,
+    "fish":0
+}
+
+let animals = Object.keys(table)
+let legs = Object.values(table)
+console.log(animals[1])
+console.log(legs[2])
+
+let question = document.getElementById('question').innerText;
+
+console.log(question)
+
+questionNew = document.getElementById('question').textContent = "hi"
+
+
