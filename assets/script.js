132705636e0634d1c746be3c49d67a07805b5f8c
RimPeter <hull@localappliancerentals.co.uk>
2023-12-30 10:37:56 +0000

    change javascript for input field

--- assets/script.js
+++ assets/script.js
@@ -19,28 +19,22 @@ let legs = Object.values(table)
 console.log("The question is: " + animals[randomNum])
 console.log("The answer is: " + legs[randomNum])
 
+
 // replace new question
 document.getElementById('question').textContent = animals[randomNum]
 
 // eventlistener to input field
-document.addEventListener("DOMContentLoaded", function() {    
-    let form = document.querySelector('.form-box');
-    form.onsubmit = function(event) {        
-        event.preventDefault();      
-        var inputValue = document.querySelector('.form-box input[type="text"]').value;
-        console.log("Input Value is : " + inputValue);
+document.querySelector('.form-box').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    var inputValue = document.getElementById('userInput').value; // Get the value of the input field
+    console.log("Input Value is : " + inputValue);
         if (inputValue == legs[randomNum]) {
             console.log(true + "; the input and the answer are the same")
+            
         } else {
             console.log(false + "; the input and the answer are NOT thesame")
         }
-            };
-    
 });
-
-
-
-
 
 
 
