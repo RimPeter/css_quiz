9f4c5255d44333802c9b67ec0d056caba114ff47
RimPeter <hull@localappliancerentals.co.uk>
2023-12-30 10:06:15 +0000

    change javascript for input field

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,4 @@
-// console.log('hi')
 
-// for (let i = 0; i < 20; i++) {
-//     setTimeout(function() {
-//         console.log(i);
-//     }, i * 1000);
-// }
 
 
 // question list
@@ -15,29 +9,43 @@ let table = {
     "fish":0
 }
 
+//generate random number for the question
+randomNum = Math.floor(Math.random()*4)
+console.log("The random number generated is " + randomNum)
+
 // variable for question and answer
 let animals = Object.keys(table)
 let legs = Object.values(table)
-console.log(animals[1])
-console.log(legs[2])
+console.log("The question is: " + animals[randomNum])
+console.log("The answer is: " + legs[randomNum])
 
-let question = document.getElementById('question').innerText;
-
-console.log(question)
-
-questionNew = document.getElementById('question').textContent = "hi"
-
-randomNum = Math.floor(Math.random()*4)
-console.log(randomNum)
+// replace new question
 document.getElementById('question').textContent = animals[randomNum]
-let answer = legs[randomNum]
-console.log(answer)
-// eventlistener fo input field
+
+// eventlistener to input field
 document.addEventListener("DOMContentLoaded", function() {    
     let form = document.querySelector('.form-box');
     form.onsubmit = function(event) {        
         event.preventDefault();      
         var inputValue = document.querySelector('.form-box input[type="text"]').value;
-        console.log("Input Value: " + inputValue);
-    };
+        console.log("Input Value is : " + inputValue);
+        if (inputValue == legs[randomNum]) {
+            console.log(true + "; the input and the answer are the same")
+        } else {
+            console.log(false + "; the input and the answer are NOT thesame")
+        }
+            };
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
