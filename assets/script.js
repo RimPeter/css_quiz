28d3fd52532b7324ddf466d3df63fa3b6ef3822d
RimPeter <hull@localappliancerentals.co.uk>
2024-01-01 13:55:38 +0000

    restructure javascript

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,38 @@
 
 // question list
 let table = {
-    "dog":4,
-    "centipede":100,
-    "chicken":2,
-    "fish":0
+    "2+2":4,
+    "50+50":100,
+    "1+1":2,
+    "1-1":0,
+    "2+8":10,
+    "50+150":200,
+    "1+15":16,
+    "10-1":9
 }
 
-// // generate random number for the question
-// let randomNum = Math.floor(Math.random()*4)
-// console.log("The random number generated is " + randomNum)
+
+let randomNum = Math.floor(Math.random()*4)
+    let numQuestion = Object.keys(table)
+    let numAnswer = Object.values(table)
+
+let nextQuestion = document.getElementById("questionButton");
+function buttonClick() {
+
+    console.log("next question clicked")
+    console.log("The random number generated is " + randomNum)
+    console.log("The question is: " + numQuestion[randomNum])
+    console.log("The answer is: " + numAnswer[randomNum])
+    return numAnswer[randomNum]
+};
+
+nextQuestion.addEventListener("click", buttonClick);
 
 
-// // variable for question and answer
-// let animals = Object.keys(table)
-// let legs = Object.values(table)
-// console.log("The question is: " + animals[randomNum])
-// console.log("The answer is: " + legs[randomNum])
+buttonClick()
+let theAnswer = numAnswer[randomNum]
 
-
-// // replace new question
-// document.getElementById('question').textContent = animals[randomNum]
-// var inputValue = document.getElementById('userInput').value;
-// eventlistener to input field
-
-
+console.log(theAnswer)
 
 document.querySelector('.form-box').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
@@ -40,58 +48,8 @@ document.querySelector('.form-box').addEventListener('submit', function(event) {
         }
 });
 
-// //event listener for the "NEXT QUESTION" button
-// const questionButton = document.getElementById('questionButton');
-
-// // Add an event listener to the button
-// questionButton.addEventListener('click', function() {
-//     // Set the variable to true when the button is clicked
-//     randomNum = Math.floor(Math.random()*4)
-//     document.getElementById('question').textContent = animals[randomNum]
-// console.log("The random number generated is " + randomNum)
-
-//     // You can add additional actions here if needed
-//     console.log("Button was clicked, random number is:", randomNum);
-// });
-
-// let rightAnswer = legs[randomNum];
-// console.log(rightAnswer)
-
-// function compare(A, B) {
-//     if (inputValue == rightAnswer) {
-//         console.log('it is true')
-//     } else {
-//         console.log('it is NOT true')
-//     }
-
-// }
-
-// let theResult = compare(inputValue, rightAnswer)
-
-let randomNum = Math.floor(Math.random()*4)
-    let animals = Object.keys(table)
-    let legs = Object.values(table)
-
-let nextQuestion = document.getElementById("questionButton");
-function buttonClick() {
-
-    console.log("next question clicked")
-    console.log("The random number generated is " + randomNum)
-    console.log("The question is: " + animals[randomNum])
-    console.log("The answer is: " + legs[randomNum])
-    return legs[randomNum]
-};
-
-nextQuestion.addEventListener("click", buttonClick);
-
-
-buttonClick()
-let theAnswer = legs[randomNum]
-
-console.log(theAnswer)
-
-
-
+document.getElementById('question').textContent = numQuestion[randomNum]
+let inputValue = document.getElementById('userInput').value;
 
 
 
