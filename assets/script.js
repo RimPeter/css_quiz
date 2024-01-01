976cbae98b14c94bfa77b2a18105950d6c4d3e58
RimPeter <hull@localappliancerentals.co.uk>
2024-01-01 19:38:25 +0000

    delete javascript

--- assets/script.js
+++ assets/script.js
@@ -13,43 +13,24 @@ let table = {
     "10-1":9
 }
 
-
-let randomNum = Math.floor(Math.random()*4)
-    let numQuestion = Object.keys(table)
-    let numAnswer = Object.values(table)
-
-let nextQuestion = document.getElementById("questionButton");
-function buttonClick() {
-
-    console.log("next question clicked")
-    console.log("The random number generated is " + randomNum)
-    console.log("The question is: " + numQuestion[randomNum])
-    console.log("The answer is: " + numAnswer[randomNum])
-    return numAnswer[randomNum]
-};
-
-nextQuestion.addEventListener("click", buttonClick);
+//generate random number
+let randomNum = Math.floor(Math.random()*8)
+let numQuestion = Object.keys(table)
+let numAnswer = Object.values(table)
 
 
-buttonClick()
-let theAnswer = numAnswer[randomNum]
 
-console.log(theAnswer)
 
-document.querySelector('.form-box').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-     // Get the value of the input field
-    console.log("Input Value is : " + inputValue);
-        if (inputValue == numAnswer[randomNum]) {
-            console.log(true + "; the input and the answer are the same")
-            return inputValue
-        } else {
-            console.log(false + "; the input and the answer are NOT thesame")
-        }
-});
 
-document.getElementById('question').textContent = numQuestion[randomNum]
-let inputValue = document.getElementById('userInput').value;
+
+
+
+
+
+
+
+
+
 
 
 
