8f65451df520ece3953a7fc01fc36fe9d27d10ef
RimPeter <hull@localappliancerentals.co.uk>
2024-01-08 21:49:37 +0000

    make minor changes for heights in gamezone.css

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,14 @@ document.getElementById('start').addEventListener('click', function() {
 window.location.href = 'gamezone.html';
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Create audio element
+    console.log("loaded")
+    //  var audio = new Audio('../assets/sound/start_page_soundtrack.mp3');
+    
+    // Set audio to play in loop
+    audio.loop = true;
 
+    // Play the audio
+    audio.play();
+});
