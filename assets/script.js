b8c9dac05f944c2d8a427c34ecc7be54023d637f
RimPeter <hull@localappliancerentals.co.uk>
2024-01-09 17:28:50 +0000

    soundtrack added to index.html

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,24 @@ document.getElementById('start').addEventListener('click', function() {
 window.location.href = 'gamezone.html';
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Create audio element
-    console.log("loaded")
-    //  var audio = new Audio('../assets/sound/start_page_soundtrack.mp3');
-    
-    // Set audio to play in loop
-    audio.loop = true;
 
-    // Play the audio
-    audio.play();
+
+document.addEventListener("DOMContentLoaded", function () {
+  let audio = document.getElementById("startmusic");
+
+  audio.loop = true;
+  console.log("sound");
+  audio.play();
+
+  let soundButton = document.getElementById("sound");
+
+  soundButton.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      soundButton.textContent = "Sound Off";
+    } else {
+      audio.pause();
+      soundButton.textContent = "Sound On";
+    }
+  });
 });
