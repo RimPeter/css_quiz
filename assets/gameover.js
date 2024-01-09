81697156eebb46cc8b65c72d8651b5018b07a581
RimPeter <hull@localappliancerentals.co.uk>
2024-01-09 17:15:06 +0000

    soundtrack added to gameover.html

--- assets/gameover.js
+++ assets/gameover.js
@@ -1,3 +1,23 @@
-document.getElementById('restart').addEventListener('click', function() {
-            window.location.href = 'index.html';
-        });
+document.getElementById("restart").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let audio = document.getElementById("sadmusic");
+
+  audio.loop = true;
+  console.log("sound");
+  audio.play();
+
+  let soundButton = document.getElementById("sound");
+
+  soundButton.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      soundButton.textContent = "Sound Off";
+    } else {
+      audio.pause();
+      soundButton.textContent = "Sound On";
+    }
+  });
+});
